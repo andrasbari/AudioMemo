3c808cfed1d246ef04727648992aca345c2d9b14
Bari András <baria@plixapp.hu>
2015-02-04 00:27:16 +0100

    -

--- AudioMemo/src/com/baandmazso/audiomemo/SinglePlayerActivity.java
+++ AudioMemo/src/com/baandmazso/audiomemo/SinglePlayerActivity.java
@@ -3,6 +3,7 @@ package com.baandmazso.audiomemo;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.net.Uri;
@@ -11,8 +12,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -38,10 +41,24 @@ public class SinglePlayerActivity extends Activity {
 			for (int k = 0; k < col_count; k++) {
 				final int k2 = k;
 				RelativeLayout cell = (RelativeLayout) row.getChildAt(k);
-				cell.setOnClickListener(new OnClickListener() {
+				/*
+				 * cell.setOnClickListener(new OnClickListener() {
+				 * @Override public void onClick(View v) { playSound(i2, k2); } });
+				 */
+				cell.setOnTouchListener(new OnTouchListener() {
 					@Override
-					public void onClick(View v) {
-						playSound(i2, k2);
+					public boolean onTouch(View v, MotionEvent event) {
+						switch (event.getAction()) {
+						case MotionEvent.ACTION_DOWN:
+							v.setBackgroundColor(Color.argb(255, 192, 64, 64));
+							playSound(i2, k2);
+							break;
+						case MotionEvent.ACTION_UP:
+							v.setBackgroundColor(Color.argb(0, 0, 0, 0));
+							stopSound();
+							break;
+						}
+						return true;
 					}
 				});
 				cells.add(cell);
@@ -50,24 +67,27 @@ public class SinglePlayerActivity extends Activity {
 	}
 
 	private void playSound(int row, int col) {
-		if (mp != null && mp.isPlaying()) {
-			mp.reset();
-			mp.release();
-		}
+		stopSound();
 		Log.d("playSound", String.valueOf(row) + " x " + String.valueOf(col));
 		mp = MediaPlayer.create(getApplicationContext(), R.raw.sound_1khz_44100hz_16bit_05sec);
-
+		mp.setLooping(true);
 		float rightVolume = (float) col / (float) col_count;
 		float leftVolume = 1.0f - rightVolume;
 		mp.setVolume(leftVolume, rightVolume);
 		mp.setOnCompletionListener(new OnCompletionListener() {
 			@Override
 			public void onCompletion(MediaPlayer mp) {
-				mp.reset();
-				mp.release();
-				mp = null;
+				// stopSound();
 			}
 		});
 		mp.start();
+	}
+
+	private void stopSound() {
+		if (mp != null && mp.isPlaying()) {
+			mp.reset();
+			mp.release();
+			mp = null;
+		}
 	}
 }
