cae902683c42c8de6f7489a6c9027dde26b1508d
Bari András <baria@plixapp.hu>
2015-02-03 23:06:37 +0100

    -

---
+++ AudioMemo/src/com/baandmazso/audiomemo/SinglePlayerActivity.java
@@ -0,0 +1,15 @@
+package com.baandmazso.audiomemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SinglePlayerActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_single_player);
+	}
+}
