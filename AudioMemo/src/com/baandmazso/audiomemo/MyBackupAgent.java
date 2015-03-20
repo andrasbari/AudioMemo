53ca1295171a6b4e3676b88541e800a8d6e40286
Bari András <baria@plixapp.hu>
2015-03-20 12:59:38 +0100

    -

---
+++ AudioMemo/src/com/baandmazso/audiomemo/MyBackupAgent.java
@@ -0,0 +1,24 @@
+package com.baandmazso.audiomemo;
+
+import java.io.IOException;
+
+import android.app.backup.BackupAgent;
+import android.app.backup.BackupDataInput;
+import android.app.backup.BackupDataOutput;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
+
+public class MyBackupAgent extends BackupAgent {
+	private static final String TAG = "MyBackupAgent";
+
+	@Override
+	public void onBackup(ParcelFileDescriptor oldState, BackupDataOutput data, ParcelFileDescriptor newState) throws IOException {
+		Log.d(TAG, "onBackup");
+	}
+
+	@Override
+	public void onRestore(BackupDataInput data, int appVersionCode, ParcelFileDescriptor newState) throws IOException {
+		Log.d(TAG, "onRestore");
+	}
+
+}
